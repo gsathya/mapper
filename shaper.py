82f1a7e7b5486af467be91f87123ea7dfb9108ba
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-10-04 14:27:27 -0700

    Use GeoJSON and plot it

--- shaper.py
+++ shaper.py
@@ -3,10 +3,11 @@ import os
 
 import fiona
 
-from flask import Flask, send_from_directory
-from shapely.geometry import Point, LineString, shape, MultiPolygon
+from flask import Flask, send_from_directory, request, jsonify
+from shapely.geometry import Point, LineString, shape, MultiPolygon, mapping
 
 app = Flask(__name__)
+polygons = []
 
 @app.route("/", methods=["GET"])
 def main():
@@ -18,12 +19,40 @@ def get_css(file):
     path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'static' , "css"))
     return send_from_directory(path, file)
 
-@app.route("/js/<file>", methods=["GET"])
+@app.route("/static/js/<file>", methods=["GET"])
 def get_js(file):
     path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), 'static', "js"))
     return send_from_directory(path, file)
 
-def run(data_dir, line):
+@app.route("/location")
+def get_locations():
+    counties = {}
+    counties['type'] = 'FeatureCollection'
+    counties['features'] = []
+
+    args = request.args
+
+    try:
+        start = Point(float(args.get('start_long')), float(args.get('start_lat')))
+        end = Point(float(args.get('end_long')), float(args.get('end_lat')))
+    
+        line = LineString([start, end])
+
+        for id, n in enumerate(find_counties(line)):
+            county = {}
+            print n[1]
+            county['type'] = 'Feature'
+            county['id'] = id
+            county['properties'] = {'name': n[1]}
+            county['geometry'] = mapping(n[0])
+            counties['features'].append(county)
+
+    except Exception, e:
+        print "Wrong request %s" % (e)
+
+    return jsonify(counties=counties)
+
+def load(data_dir):
     for file_name in os.listdir(data_dir):
         if not file_name.endswith(".shp"):
             continue
@@ -32,20 +61,31 @@ def run(data_dir, line):
         with fiona.open(file_path, "r") as input:
             for f in input:
                 neighborhood = shape(f['geometry'])
-                if line.intersects(neighborhood):
-                    print f['properties']['NAME']
+                polygons.append((neighborhood, f['properties']['NAME']))
+
+def find_counties(line):
+    county = []
+
+    for polygon in polygons:
+        if line.intersects(polygon[0]):
+            county.append(polygon)
+
+    return county
 
 def usage():
-    print "$python shaper.py server"
+    print "$python shaper.py server /path/to/data"
     print "$python shaper.py /path/to/data lat1 long1 lat2 long2"
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2 and sys.argv[1] == "server":
+    if len(sys.argv) == 3 and sys.argv[1] == "server":
+        data_dir = sys.argv[2]
+        load(data_dir)
         app.run(debug=True)
     elif len(sys.argv) == 6:
-        data_dir = sys.argv[1]
-
         try:
+            data_dir = sys.argv[1]
+            load(data_dir)
+        
             start = Point(float(sys.argv[2]), float(sys.argv[3]))
             end = Point(float(sys.argv[4]), float(sys.argv[5]))
             line = LineString([start, end])
@@ -53,6 +93,7 @@ if __name__ == '__main__':
             usage()
             sys.exit()
 
-        run(data_dir, line)
+        for county, name in find_counties(line):
+            print name
     else:
         usage()
--- static/js/custom.js
+++ static/js/custom.js
@@ -3,13 +3,45 @@ var osmAttr = '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contr
 var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 
 L.tileLayer(osmUrl, {
-attribution: osmAttr,
-maxZoom: 18
+    attribution: osmAttr,
+    maxZoom: 18
 }).addTo(map);
 
+var points = []
 
 function onMapClick(e) {
-    console.log("You clicked the map at " + e.latlng);
+    var marker = L.marker(e.latlng).addTo(map);
+    console.log(e.latlng);
+    points.push(e.latlng);
+
+    points_len = points.length;
+    if (points_len % 2 === 0){
+        start = points[points_len-1]
+        end = points[points_len-2]
+
+        plot_neighborhoods(start, end);
+    }
+}
+
+function plot_neighborhoods(start, end){
+    // create a red polyline from an arrays of LatLng points
+    var polyline = L.polyline([start, end], {color: 'red'}).addTo(map);
+
+    // zoom the map to the polyline
+    map.fitBounds(polyline.getBounds());
+
+    var data = {
+        'start_lat': start.lat,
+        'start_long': start.lng,
+        'end_lat': end.lat,
+        'end_long': end.lng
+    }
+    $.get("/location", data)
+        .done(function(data){
+            console.log(data['counties']);
+            L.geoJson(data['counties']).addTo(map);
+        });
+
 }
 
 map.on('click', onMapClick);
