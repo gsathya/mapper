0f8818608424861d7a3ada17b9f512add6aab7d7
Sathyanarayanan Gunasekaran <gsathya.ceg@gmail.com>
2014-10-04 10:34:19 -0700

    Find me some counties

--- shaper.py
+++ shaper.py
@@ -2,17 +2,18 @@ import sys
 
 import fiona
 
-from shapely.geometry import Point, LineString
+from shapely.geometry import Point, LineString, shape, MultiPolygon
 
 def run():
-    start = Point(33.7758, 84.3947)
-    end = Point(32.0784, -81.0988)
+    start = Point(-84.395953, 33.777085)
+    end = Point(-84.280820, 33.797427)
     line = LineString([start, end])
 
-    print line
     with fiona.open("GA/ZillowNeighborhoods-GA.shp", "r") as input:
         for f in input:
-            print f
+            neighborhood = shape(f['geometry'])
+            if line.intersects(neighborhood):
+                print f['properties']['NAME']
 
 if __name__ == '__main__':
     run()
